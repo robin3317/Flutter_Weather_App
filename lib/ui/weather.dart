1a6ddfc8cc54b7ff56648101b99e9c0f0799829a
robin3317 <abdurrahmanrobin47@gmail.com>
2018-07-24 04:15:21 +0600

    print data from api on the console

--- lib/ui/weather.dart
+++ lib/ui/weather.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+
+final appId = "c85a08c4c0785a7b6f225056bc4f39c5";
+final defaultCity = "Dhaka";
 
 class WeatherUpdate extends StatefulWidget {
   @override
@@ -6,6 +13,13 @@ class WeatherUpdate extends StatefulWidget {
 }
 
 class _WeatherUpdateState extends State<WeatherUpdate> {
+
+//get data and print it on the console
+  void showData() async {
+    Map data = await getWeather(appId, defaultCity);
+    print(data.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -16,7 +30,7 @@ class _WeatherUpdateState extends State<WeatherUpdate> {
         actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.menu),
-            onPressed: null,
+            onPressed: showData,
           )
         ],
       ),
@@ -35,11 +49,29 @@ class _WeatherUpdateState extends State<WeatherUpdate> {
             alignment: Alignment.topRight,
             margin: const EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 0.0),
             child: new Text("Dhaka", style: cityStyle(),),
-          )
+          ),
+          //this container will contain the actual data
+          new Container(
+            alignment:Alignment.center,
+            margin: const EdgeInsets.fromLTRB(0.0, 0.0, 20.0, 180.0),
+            child: new Text(
+              "45.5F",
+              style: new TextStyle(
+                fontSize: 28.90,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),
+            ),
+          ) 
         ],
       )
-      
+
     );
+  }
+  Future<Map> getWeather(String appId, String city) async {
+    String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$appId&units=metric";
+    http.Response response = await http.get(apiUrl);
+    return json.decode(response.body);
   }
 }
 
@@ -48,6 +80,5 @@ TextStyle cityStyle() {
   return new TextStyle(
     color: Colors.white,
     fontSize: 30.0,
-    fontWeight: FontWeight.bold
   );
 }
