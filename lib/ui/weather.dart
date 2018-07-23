4e35e465b1ff2ed5599e594d82aacd0f02f10d5c
robin3317 <abdurrahmanrobin47@gmail.com>
2018-07-24 02:52:07 +0600

    add image background and home is well designed

--- lib/ui/weather.dart
+++ lib/ui/weather.dart
@@ -9,8 +9,10 @@ class _WeatherUpdateState extends State<WeatherUpdate> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+
       appBar: new AppBar(
         title: new Text("Weather Update"),
+        backgroundColor: Colors.red,
         actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.menu),
@@ -18,6 +20,34 @@ class _WeatherUpdateState extends State<WeatherUpdate> {
           )
         ],
       ),
+
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset(
+              'images/image3.jpg', 
+              fit: BoxFit.fill,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+          ),
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0.0, 15.0, 15.0, 0.0),
+            child: new Text("Dhaka", style: cityStyle(),),
+          )
+        ],
+      )
+      
     );
   }
+}
+
+//text style
+TextStyle cityStyle() {
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 30.0,
+    fontWeight: FontWeight.bold
+  );
 }
